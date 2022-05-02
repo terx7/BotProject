13ce388558b709d381d73b41148d78475839badc
terx7 <erkileemet10@gmail.com>
2022-05-03 00:33:33 +0300

    added music player with play, skip and stop commands

---
+++ src/main/java/me/erki/testBot/Commands/music/PlayCommand.java
@@ -0,0 +1,90 @@
+package me.erki.testBot.Commands.music;
+
+import me.erki.testBot.LavaPlayer.PlayerManager;
+import me.erki.testBot.Utils.CommandExecutor;
+import net.dv8tion.jda.api.entities.AudioChannel;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+public class PlayCommand implements CommandExecutor {
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+
+        final MessageChannel channel = event.getChannel();
+        final Member self = event.getGuild().getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        if (args.length == 0){
+            channel.sendMessage("Correct usage is `-play <youtube link>`").queue();
+            return true;
+        }
+
+        final Member member = event.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+
+//        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+//            channel.sendMessage("You need to be in the same voice channel as me for this to work").queue();
+//            return true;
+//        }
+
+        if(selfVoiceState.inAudioChannel()){
+            channel.sendMessage("I'm already in a voice channel.").queue();
+            return true;
+        }
+
+        if (!memberVoiceState.inAudioChannel()) {
+            channel.sendMessage("You need to be in a voice channel for this command to work.").queue();
+            return true;
+        }
+
+        final AudioManager audioManager = event.getGuild().getAudioManager();
+        final AudioChannel memberChannel = memberVoiceState.getChannel();
+
+        audioManager.openAudioConnection(memberChannel);
+
+
+        String link = String.join(" ", args);
+
+        if(!isUrl(link)){
+            link = "ytsearch:" + link;
+        }
+
+        PlayerManager.getInstance()
+                .loadAndPlay(event, channel, link);
+
+        return true;
+    }
+
+    @Override
+    public String name() {
+        return "play";
+    }
+
+    @Override
+    public String description() {
+        return "play";
+    }
+
+    @Override
+    public String alias() {
+        return "play";
+    }
+
+    private boolean isUrl(String url){
+        try {
+            new URI(url);
+            return true;
+        }catch (URISyntaxException e){
+            return false;
+        }
+    }
+}
---
+++ src/main/java/me/erki/testBot/Commands/music/SkipCommand.java
@@ -0,0 +1,56 @@
+package me.erki.testBot.Commands.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import me.erki.testBot.LavaPlayer.GuildMusicManager;
+import me.erki.testBot.LavaPlayer.PlayerManager;
+import me.erki.testBot.Utils.CommandExecutor;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+@SuppressWarnings("ConstantConditions")
+public class SkipCommand implements CommandExecutor {
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+
+        final MessageChannel channel = event.getChannel();
+        final Member self = event.getGuild().getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        final Member member = event.getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+            channel.sendMessage("You need to be in the same voice channel as me for this to work").queue();
+            return true;
+        }
+
+        final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
+        final AudioPlayer audioPlayer = musicManager.audioPlayer;
+
+        if (audioPlayer.getPlayingTrack() == null) {
+            channel.sendMessage("There is no track playing currently.").queue();
+            return true;
+        }
+
+        musicManager.scheduler.nextTrack();
+
+        return true;
+    }
+
+    @Override
+    public String name() {
+        return "skip";
+    }
+
+    @Override
+    public String description() {
+        return "skip";
+    }
+
+    @Override
+    public String alias() {
+        return "skip";
+    }
+}
