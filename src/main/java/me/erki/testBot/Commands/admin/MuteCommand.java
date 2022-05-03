68835aebd0760e7e9ac9437a485e927aa9b698b7
kennethlember <kennethlember@gmail.com>
2022-05-03 09:31:03 +0300

    Create mute command

---
+++ src/main/java/me/erki/testBot/Commands/admin/MuteCommand.java
@@ -0,0 +1,59 @@
+package me.erki.testBot.Commands.admin;
+
+import me.erki.testBot.Utils.CommandExecutor;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+
+public class MuteCommand implements CommandExecutor {
+
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+        MessageChannel channel = event.getChannel();
+        boolean isModerator = event.getMember().getRoles().toString().toLowerCase().contains("moderator");
+        boolean isAdmin = event.getMember().getRoles().toString().toLowerCase().contains("admin");
+        boolean isOwner = event.getMember().isOwner();
+
+        Duration muteDuration = Duration.ofMinutes(parseInt(Arrays.stream(args).toList().get(args.length - 1)));
+
+        if(isAdmin || isOwner || isModerator){
+            if(args.length == 0){ //no argument
+                //error message
+                channel.sendMessage("no user given").queue();
+                return true;
+            }else{
+                List<User> muteList = event.getJDA().getUsers();
+
+                for (User i : muteList) {
+                    System.out.println(i.getId());
+                    {
+                        event.getGuild().timeoutFor((Member) i, muteDuration).queue();
+                        channel.sendMessage(i.getAsTag() + " was muted for" + muteDuration + " minutes!").queue();
+                    }
+                    return true;
+                }
+
+            }
+
+        }
+        return true;
+    }
+
+
+    @Override
+    public String name() { return "Mute"; }
+
+    @Override
+    public String description() { return "Times a user out/Mutes a user."; }
+
+    @Override
+    public String alias() { return "Mute"; }
+}
