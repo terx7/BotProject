a745cff89b70282a1e9859b91de1a1292de719e4
kennethlember <kennethlember@gmail.com>
2022-05-03 15:09:32 +0300

    Create role creation command

---
+++ src/main/java/me/erki/testBot/Commands/admin/CreateRoleCommand.java
@@ -0,0 +1,47 @@
+package me.erki.testBot.Commands.admin;
+
+import me.erki.testBot.Utils.CommandExecutor;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateRoleCommand implements CommandExecutor {
+
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+        MessageChannel channel = event.getChannel();
+        boolean isAdmin = event.getMember().getRoles().toString().toLowerCase().contains("admin");
+        boolean isOwner = event.getMember().isOwner();
+        if (isAdmin || isOwner) {
+//            if(args.length == 0 ) { //no argument
+//                //error message
+//                channel.sendMessage("No arguments given").queue();
+//                return true;
+//            }
+            event.getGuild().createRole()
+                    .setName("Tolgus")
+                    .setColor(Color.red)
+                    .setHoisted(true)
+                    .setMentionable(false)
+                    .queue(role -> {
+                        System.out.println("Created role");
+                    });
+        }
+        return true;
+    }
+
+    @Override
+    public String name () { return "AddRole"; }
+
+    @Override
+    public String description () { return "Creates a role."; }
+
+    @Override
+    public String alias () { return "AddRole"; }
+}
