670f9ef7982d13dd26a5ae1de16bd6ee1f979ab3
erki.leemet <t3rx789@gmail.com>
2022-05-05 14:03:04 +0300

    added queue command

---
+++ src/main/java/me/erki/testBot/Commands/music/QueueCommand.java
@@ -0,0 +1,83 @@
+package me.erki.testBot.Commands.music;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import me.erki.testBot.LavaPlayer.GuildMusicManager;
+import me.erki.testBot.LavaPlayer.PlayerManager;
+import me.erki.testBot.Utils.CommandExecutor;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class QueueCommand implements CommandExecutor {
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+        final MessageChannel channel = event.getChannel();
+        final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(event.getGuild());
+        final BlockingQueue<AudioTrack> queue = musicManager.scheduler.queue;
+
+
+        if (queue.isEmpty()) {
+            channel.sendMessage("The queue is currently empty").queue();
+            return true;
+        }
+
+        final int trackCount = Math.min(queue.size(), 20);
+        final List<AudioTrack> trackList = new ArrayList<>(queue);
+        final MessageAction messageAction = channel.sendMessage("**Current Queue:**\n");
+
+        for (int i = 0; i <  trackCount; i++) {
+            final AudioTrack track = trackList.get(i);
+            final AudioTrackInfo info = track.getInfo();
+
+            messageAction.append('#')
+                    .append(String.valueOf(i + 1))
+                    .append(" `")
+                    .append(String.valueOf(info.title))
+                    .append(" by ")
+                    .append(info.author)
+                    .append("` [`")
+                    .append(formatTime(track.getDuration()))
+                    .append("`]\n");
+        }
+
+        if (trackList.size() > trackCount) {
+            messageAction.append("And `")
+                    .append(String.valueOf(trackList.size() - trackCount))
+                    .append("` more...");
+        }
+
+        messageAction.queue();
+
+
+        return true;
+    }
+
+    private String formatTime (long timeInMillis) {
+        final long hours = timeInMillis / TimeUnit.HOURS.toMillis(1);
+        final long minutes = timeInMillis / TimeUnit.MINUTES.toMillis(1);
+        final long seconds = timeInMillis % TimeUnit.MINUTES.toMillis(1) / TimeUnit.SECONDS.toMillis(1);
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    @Override
+    public String name() {
+        return "queue";
+    }
+
+    @Override
+    public String description() {
+        return "shows all the tracks currently in queue";
+    }
+
+    @Override
+    public String alias() {
+        return "queue";
+    }
+}
