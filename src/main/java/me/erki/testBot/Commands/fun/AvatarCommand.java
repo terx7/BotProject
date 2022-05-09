f36990533d0a509b015c4313977e90a5d779972e
erki.leemet <t3rx789@gmail.com>
2022-05-09 14:45:08 +0300

    tweaked queue and avatar commands

--- src/main/java/me/erki/testBot/Commands/fun/AvatarCommand.java
+++ src/main/java/me/erki/testBot/Commands/fun/AvatarCommand.java
@@ -5,15 +5,27 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import javax.swing.plaf.metal.MetalMenuBarUI;
 import java.util.List;
 
 public class AvatarCommand implements CommandExecutor {
     @Override
     public boolean execute(String[] args, MessageReceivedEvent event) {
         MessageChannel channel = event.getChannel();
-        List<Member> userList = event.getMessage().getMentionedMembers();
-        String avatar = userList.get(1).getAvatarUrl();
-        channel.sendMessage(avatar).queue();
+
+        if(args.length > 0){
+            List<Member> userList = event.getMessage().getMentionedMembers();
+            String avatar = userList.get(0).getUser().getAvatarUrl();
+            channel.sendMessage(avatar).queue();
+            return true;
+        }
+
+        if (args.length == 0){
+            String avatar = event.getAuthor().getAvatarUrl();
+            channel.sendMessage(avatar).queue();
+            return true;
+        }
+        channel.sendMessage("No avatar available.").queue();
         return true;
     }
 
