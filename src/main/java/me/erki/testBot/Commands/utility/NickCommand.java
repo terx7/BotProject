7d56e2d9beaeaa078517284eadf91591d2d206d8
erki.leemet <t3rx789@gmail.com>
2022-05-10 09:43:03 +0300

    tweaked change nickname command

--- src/main/java/me/erki/testBot/Commands/utility/NickCommand.java
+++ src/main/java/me/erki/testBot/Commands/utility/NickCommand.java
@@ -17,17 +17,15 @@ public class NickCommand implements CommandExecutor {
         boolean hasPermission = isAdmin2.contains(Permission.NICKNAME_CHANGE);
 
         if(hasPermission){
-            String user = event.getAuthor().getAsMention();
+            Member user = event.getMember();
             if(args.length == 0) { //no argument
                 //error message
                 channel.sendMessage("no nickname change given").queue();
                 return true;
-            } else if (args.length == 1) {
-                event.getGuild().getMember(User.fromId(user)).modifyNickname(args[0]);
-                return true;
             } else {
-                String newNick = String.valueOf(event.getMessage()).replace("-nick ", "");
-                event.getGuild().getMember(User.fromId(user)).modifyNickname(newNick);
+                String newNick = event.getMessage().getContentRaw().replace("-nick ", "");
+                System.out.println(newNick);
+                user.modifyNickname(newNick).queue();
             }
 
         }
